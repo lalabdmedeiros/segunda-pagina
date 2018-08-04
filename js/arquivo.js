3d648f2320d7b1079937cc8dfa829530a9ea66a3
dimytrius <dimy.rocha@gmail.com>
2018-08-04 08:56:24 -0300

    primeiro commit

---
+++ js/arquivo.js
@@ -0,0 +1,9 @@
+
+function enviar(){
+var name = document.getElementById('txt-nome');
+if(name.value !=" "){
+    alert("Obrigado "+ name.value +"! Mas esta área não esta disponivel");
+}else{
+    alert("Obrigado! Mas essa área não esta disponivel");
+}
+}
